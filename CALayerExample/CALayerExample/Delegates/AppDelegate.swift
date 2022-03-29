617036efeb709bf9b7de6b7625e8c048b31da7b0
ankonovalov <anatoliyk@wizardsdev.com>
2022-03-29 21:45:48 +0300

    Refactoring and Structuring of project

--- CALayerExample/CALayerExample/Delegates/AppDelegate.swift
+++ CALayerExample/CALayerExample/Delegates/AppDelegate.swift
@@ -1,16 +1,7 @@
-//
-//  AppDelegate.swift
-//  CALayerExample
-//
-//  Created by Anatoliy Konovalov on 29.03.2022.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
